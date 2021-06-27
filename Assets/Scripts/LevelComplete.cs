663b90f7a68c6e254cfdcab35f3c58f0a168a6a5
DuckSplash <davelister00169@gmail.com>
2021-06-27 14:39:39 +0000

    UI Changes

--- Assets/Scripts/LevelComplete.cs
+++ Assets/Scripts/LevelComplete.cs
@@ -21,73 +21,79 @@ public class LevelComplete : MonoBehaviour
 	public Text scoreTextTotal;
 	public int tempLevel = 1;
 
+	private Boolean levelOver = false;
+
 	
 	void OnTriggerEnter2D(Collider2D other)
-	{   
-
+	{
+		if (!levelOver)
+		{
 
 			// Process end of level
 			if (other.name.Contains("Player"))
 			{
-			
-			CountUpTimer.clockStopped = true;
-			
-			dialogBoxPanel.GetComponent<CanvasGroup>().alpha = 1.0f;
-			dialogBoxPanel.GetComponent<CanvasGroup>().blocksRaycasts = true;	
-			
-			levelEndStats.GetComponent<CanvasGroup>().alpha = 1.0f;
-			levelEndStats.GetComponent<CanvasGroup>().blocksRaycasts = true;	
-			
-			PausedPane.GetComponent<CanvasGroup>().alpha = 0.0f;
-			PausedPane.GetComponent<CanvasGroup>().blocksRaycasts = false;	
-			
-			QuitPane.GetComponent<CanvasGroup>().alpha = 0.0f;
-			QuitPane.GetComponent<CanvasGroup>().blocksRaycasts = false;	
-			
-			PauseButton.GetComponent<CanvasGroup>().alpha = 0.0f;
-			PauseButton.GetComponent<CanvasGroup>().blocksRaycasts = false;	
-			
-			QuitButton.GetComponent<CanvasGroup>().alpha = 0.0f;
-			QuitButton.GetComponent<CanvasGroup>().blocksRaycasts = false;	
-			
-			
-			titleText.text = "Level "+PlayerStats.CURRENT_LEVEL+" Complete!";
-			
-			timeText.text = CountUpTimer.finalTime.ToString();
-			
-			
-			// potential score for level is the level number times 100
-			var potentialScore = PlayerStats.CURRENT_LEVEL * 100;
-			
-			var countFinalTime = Convert.ToInt32(CountUpTimer.finalTime);
-			
-			var scoreMaffs = 0;
-			
-			if (countFinalTime < 101)
-			{
-				scoreMaffs = potentialScore - countFinalTime;				
+
+				CountUpTimer.clockStopped = true;
+
+				dialogBoxPanel.GetComponent<CanvasGroup>().alpha = 1.0f;
+				dialogBoxPanel.GetComponent<CanvasGroup>().blocksRaycasts = true;
+
+				levelEndStats.GetComponent<CanvasGroup>().alpha = 1.0f;
+				levelEndStats.GetComponent<CanvasGroup>().blocksRaycasts = true;
+
+				PausedPane.GetComponent<CanvasGroup>().alpha = 0.0f;
+				PausedPane.GetComponent<CanvasGroup>().blocksRaycasts = false;
+
+				QuitPane.GetComponent<CanvasGroup>().alpha = 0.0f;
+				QuitPane.GetComponent<CanvasGroup>().blocksRaycasts = false;
+
+				PauseButton.GetComponent<CanvasGroup>().alpha = 0.0f;
+				PauseButton.GetComponent<CanvasGroup>().blocksRaycasts = false;
+
+				QuitButton.GetComponent<CanvasGroup>().alpha = 0.0f;
+				QuitButton.GetComponent<CanvasGroup>().blocksRaycasts = false;
+
+
+				titleText.text = "Level " + PlayerStats.CURRENT_LEVEL + " Complete!";
+
+				timeText.text = CountUpTimer.finalTime.ToString();
+
+
+				// potential score for level is the level number times 100
+				var potentialScore = PlayerStats.CURRENT_LEVEL * 100;
+
+				var countFinalTime = Convert.ToInt32(CountUpTimer.finalTime);
+
+				var scoreMaffs = 0;
+
+				if (countFinalTime < 101)
+				{
+					scoreMaffs = potentialScore - countFinalTime;
+				}
+				else
+				{
+					scoreMaffs = 0;
+				}
+
+
+
+				PlayerStats.PLAYER_SCORE += scoreMaffs;
+
+
+
+				scoreText.text = scoreMaffs.ToString();
+
+
+				scoreTextTotal.text = (PlayerStats.PLAYER_SCORE).ToString();
+
+				// stop play here
+
+				levelOver = true;
+				Time.timeScale = 0;
+
 			}
-			else
-			{
-				scoreMaffs = 0;	
-			}
 
-			
-
-			PlayerStats.PLAYER_SCORE += scoreMaffs;
-
-			
-			
-			scoreText.text = scoreMaffs.ToString();
-			
-			
-			scoreTextTotal.text = (PlayerStats.PLAYER_SCORE).ToString();
-			
-			// stop play here
-			Time.timeScale = 0;
-			}
-
-
+		}
 
 	}
 
